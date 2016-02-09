6597289ed3d50222caf33d83a4a9467283f615d0
ziarno7 <filip@jarno.pl>
2016-02-09 22:07:00 +0100

    create schema for Notifications collection

--- collections/Notifications.jsx
+++ collections/Notifications.jsx
@@ -4,3 +4,94 @@
 Notifications = new Mongo.Collection('notifications');
 Notifications.permit(['insert', 'update', 'remove']).never().apply(); //ongoworks:security
 
+/**
+ * SCHEMAS
+ */
+Notifications.Schemas = {};
+Notifications.Schemas.Main = new SimpleSchema({
+  type: {
+    type: String,
+    allowedValues: ['comment', 'event', 'present']
+  },
+  action: {
+    type: String,
+    allowedValues: ['new', 'changed', 'edited']
+  },
+  date: {
+    type: String,
+    autoValue() {
+      if (!this.isSet) {
+        return new Date();
+      }
+    }
+  },
+
+  //users to be notified
+  seenByUsers: {
+    type: [Object]
+  },
+  'seenByUsers.id': {
+    type: String
+  },
+  'seenByUsers.seen': {
+    type: Boolean
+  },
+
+  //user who initiated the notification
+  byUser: {
+    type: Object
+  },
+  'byUser.id': {
+    type: String
+  },
+  'byUser.name': {
+    type: String
+  },
+  'byUser.picture': {
+    type: String
+  },
+
+  //user who the notification is about (ex. present -> for user)
+  forUser: {
+    type: Object,
+    optional: true
+  },
+  'forUser.id': {
+    type: String
+  },
+  'forUser.name': {
+    type: String
+  },
+  'forUser.picture': {
+    type: String
+  },
+
+  //present that the notification is about
+  forPresent: {
+    type: Object,
+    optional: true
+  },
+  'forPresent.id': {
+    type: String
+  },
+  'forPresent.name': {
+    type: String
+  },
+  'forPresent.picture': {
+    type: String
+  },
+
+  //event that the notification is about
+  forEvent: {
+    type: Object,
+    optional: true
+  },
+  'forEvent.id': {
+    type: String
+  },
+  'forEvent.name': {
+    type: String
+  }
+});
+
+Notifications.attachSchema(Notifications.Schemas.Main);
