8d292ce633b52507c811699c5bf758223d1b37f4
ziarno7 <filip@jarno.pl>
2016-02-22 11:48:01 +0100

    move navigation container component to header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,13 +1,37 @@
 Header = React.createClass({
   mixins: [ReactMeteorData],
   getMeteorData() {
-    return {};
+    var eventsSubscribtion = Meteor.subscribe('events');
+
+    return {
+      ready: eventsSubscribtion.ready(),
+      events: Events.find().fetch(),
+      user: Meteor.user()
+    };
+  },
+  getLoggedInNavigation() {
+    return (
+      <div id="navigation-container">
+        <EventsButton events={this.data.events} ready={this.data.ready} />
+        <UserProfileButton {...this.data.user.profile} />
+        <NotificationIcon />
+      </div>
+    );
+  },
+  getLoggedOutNavigation() {
+    return (
+      <div id="navigation-container">
+        <LoginOrRegisterButton />
+      </div>
+    );
   },
   render() {
     return (
       <div className="app-header shadow no-selection">
         <h1 className="title no-selection">Prezentowo</h1>
-        <NavigationContainer />
+        {this.data.user ?
+          this.getLoggedInNavigation() :
+          this.getLoggedOutNavigation()}
       </div>
     );
   }
