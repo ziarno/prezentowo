941f0ebacb198fa0a82cf71b785657fef5773f0c
ziarno7 <filip@jarno.pl>
2016-03-04 06:08:51 +0100

    style image picker

--- components/ImagePicker/ImagePicker.jsx
+++ components/ImagePicker/ImagePicker.jsx
@@ -1,7 +1,8 @@
 ImagePicker = React.createClass({
 
   propTypes: {
-    onChange: React.PropTypes.func.isRequired
+    onChange: React.PropTypes.func.isRequired,
+    images: React.PropTypes.array
   },
 
   getInitialState() {
@@ -27,11 +28,34 @@ ImagePicker = React.createClass({
 
   render() {
     return (
-      <div className="image-picker">
-        <input
-          type="file"
-          onChange={this.sendImage}
+      <div className="image-picker shadow">
+        <div className="image">
+          <div className="arrow arrow--left">
+            <i className="chevron left icon"></i>
+          </div>
+          <div className="arrow arrow--right">
+            <i className="chevron right icon"></i>
+          </div>
+        </div>
+        <div className="ui compact small buttons image-picker--actions">
+          <label
+            htmlFor="picture"
+            className="ui icon right labeled button
+              image-picker--upload waves-effect waves-button">
+            <T>Upload file</T>
+            <i className="upload icon"></i>
+          </label>
+          <input
+            name="picture"
+            id="picture"
+            type="file"
+            onChange={this.sendImage}
           />
+          <div className="ui icon button waves-effect waves-button">
+            <i className="search icon"></i>
+          </div>
+        </div>
+
       </div>
     );
   }
