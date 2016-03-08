20728551eb674abb6e5044f667c10cd688b11334
ziarno7 <filip@jarno.pl>
2016-03-08 19:51:52 +0100

    METEOR 1.3-beta.12 UPDATE

--- components/DateField/DateField.jsx
+++ components/DateField/DateField.jsx
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 DateField = React.createClass({
 
   mixins: [Mixins.Tooltips],
--- lib/Language.jsx
+++ lib/Language.jsx
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 Language = {
 
   currentLanguage: 'pl',
