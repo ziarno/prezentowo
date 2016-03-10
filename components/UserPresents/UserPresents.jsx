62d989868b57a83090699c54cff2d70b3d746df3
ziarno7 <filip@jarno.pl>
2016-03-10 16:30:15 +0100

    create 'add present' and 'toggle' icons

--- components/UserPresents/UserPresents.jsx
+++ components/UserPresents/UserPresents.jsx
@@ -10,6 +10,15 @@ UserPresents = React.createClass({
       <div className="user-presents">
         <div className="ui horizontal divider">
           <User user={this.props.user} />
+          <div className="circular ui icon button left waves-effect waves-button">
+            <i className="large icons">
+              <i className="gift icon"></i>
+              <i className="plus corner icon"></i>
+            </i>
+          </div>
+          <div className="ui icon button right waves-effect waves-button">
+            <i className="chevron up icon"></i>
+          </div>
         </div>
 
         {this.props.presents.length ? this.props.presents.map((present) => (
