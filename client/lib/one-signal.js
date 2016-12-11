f003b7fb38de000d93c79d34a5c75e477abbd4ef
Filip Jarno <filip@jarno.pl>
2016-12-11 10:27:29 +0100

    add one signal to client

---
+++ client/lib/one-signal.js
@@ -0,0 +1,13 @@
+OneSignal = window.OneSignal || []
+
+OneSignal.push(["init", {
+  appId: "f0b8d2c2-1848-4ac6-8075-96756fd0d773",
+  autoRegister: true,
+  subdomainName: 'prezentowo',
+  httpPermissionRequest: {
+    enable: true
+  },
+  notifyButton: {
+    enable: true
+  }
+}])
